0119eab5e47b627db203684c1995afd1b65f91c7
TAKETODAY <2228592280@qq.com>
2019-12-30 21:44:07 +0800

    :fire: 丢弃参数调用

--- src/main/java/cn/taketoday/invoker/Invoker.java
+++ src/main/java/cn/taketoday/invoker/Invoker.java
@@ -26,10 +26,6 @@ package cn.taketoday.invoker;
 @FunctionalInterface
 public interface Invoker {
 
-    default Object invoke() {
-        return invoke(null, null);
-    }
-
     Object invoke(Object obj, Object[] args);
 
 }
